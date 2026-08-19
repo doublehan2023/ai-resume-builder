d3b90193a67f5c12d01a44c048a5fa8538c3ec23
Han Wang <hanw8950@gmail.com>
2026-08-19 14:05:56 -0400

    add professional summary component

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -16,6 +16,7 @@ import PersonalInfoForm from "../components/PersonalInfoForm";
 import ResumePreview from "../components/ResumePreview";
 import TemplateSelector from "../components/TemplateSelector";
 import ColorPicker from "../components/ColorPicker";
+import ProfessionalSummaryForm from "../components/ProfessionalSummaryForm";
 
 const ResumeBuilder = () => {
   const { resumeId } = useParams();
@@ -164,6 +165,11 @@ const ResumeBuilder = () => {
                     removeBackground={removeBackground}
                     setRemoveBackground={setRemoveBackground}
                   />
+                )}
+                {activeSection.id === "summary" && (
+                  <ProfessionalSummaryForm data={resumeData.professional_summary}
+                  onChange={(data)=> setResumeData(prev=> ({...prev, professional_summary: data}))}
+                  setResumeData={setResumeData}/>
                 )}
               </div>
             </div>
