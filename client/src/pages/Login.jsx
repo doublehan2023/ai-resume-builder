198a0b675f8935db2c2636563492be2ee77ccb12
Han Wang <hanw8950@gmail.com>
2026-08-27 20:36:50 -0400

    Redirect users after authentication

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import { Lock, Mail, User2Icon } from 'lucide-react'
 import React from 'react'
 import api from '../configs/api'
 import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import { login } from '../app/features/authSlice'
 import toast from 'react-hot-toast'
 
@@ -9,6 +10,7 @@ import toast from 'react-hot-toast'
 const Login = () => {
 
     const dispatch = useDispatch()
+    const navigate = useNavigate()
   const query = new URLSearchParams(window.location.search)
   const urlState = query.get('state')
   const [state, setState] = React.useState(urlState || "login")
@@ -31,6 +33,7 @@ const Login = () => {
             dispatch(login(data))
             localStorage.setItem('token', data.token)
             toast.success(data.message)
+            navigate('/app')
         } catch (error) {
             toast(error?.response?.data?.message || error.message)
         }
